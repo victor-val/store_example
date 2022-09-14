79106484ec4a4cf8d2445567ab2efd98c2943f28
victor-val <vval26v@gmail.com>
2022-09-14 12:42:49 +0200

    product list component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      { path: '', component: TopBarComponent },
+      { path: '', component: ProductListComponent },
     ])
   ],
   providers: [],
